70f88bf1cbd48553490f364be9ff1c693c1da3b7
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-10-13 20:50:07 -0300

    Primeiro commit

---
+++ GerenciarProjetos/GerenciarProjetos/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    // Interface para funcionários que podem gerenciar projetos
+    public interface IManager
+    {
+        void GerenciarProjeto();
+    }
+
+    // Interface para funcionários que podem programar
+    public interface IProgrammer
+    {
+        void Programar();
+    }
+
+    // Classe base para funcionários
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+    }
+
+    // Classe que implementa a interface IManager
+    public class Gerente : Funcionario, IManager
+    {
+        public void GerenciarProjeto()
+        {
+            Console.WriteLine($"{Nome} está gerenciando um projeto.");
+        }
+    }
+
+    // Classe que implementa a interface IProgrammer
+    public class Programador : Funcionario, IProgrammer
+    {
+        public void Programar()
+        {
+            Console.WriteLine($"{Nome} está programando.");
+        }
+    }
+}
